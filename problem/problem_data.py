ffa259f92731d366cfc4fc96c60b5e62c6316cf6
Henrique Desktop <henrique2204barbosa@gmail.com>
2025-06-08 18:04:08 -0300

    first commit

---
+++ problem/problem_data.py
@@ -0,0 +1,10 @@
+import dataclasses
+
+@dataclasses.dataclass
+class ProblemData:
+    """
+    Class to hold the problem data for the optimization problem.
+    """
+
+    # Problem data
+    n: int = 0
